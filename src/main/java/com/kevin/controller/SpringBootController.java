1e2190b2f13f94eb8bd311d961e45b51201786a2
qingwei201314 <87863823@qq.com>
2018-08-23 19:17:54 +0800

    1

--- src/main/java/com/kevin/controller/SpringBootController.java
+++ src/main/java/com/kevin/controller/SpringBootController.java
@@ -1,10 +1,8 @@
 package com.kevin.controller;
 
 import javax.annotation.Resource;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.kevin.dto.City;
 import com.kevin.mapper.CityMapper;
 
@@ -15,6 +13,7 @@ public class SpringBootController {
 
 	@RequestMapping("/")
 	public City home() {
+		// 现在在company分支，看能不能提交成功
 		String name = "北京市";
 		City city = cityMapper.find(name);
 		return city;

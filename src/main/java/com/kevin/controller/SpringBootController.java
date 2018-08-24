a2b6df5de8d6c9e1ff8dd2647ea6913e46bf6241
qingwei201314 <87863823@qq.com>
2018-08-24 09:45:19 +0800

    conflict 111

--- src/main/java/com/kevin/controller/SpringBootController.java
+++ src/main/java/com/kevin/controller/SpringBootController.java
@@ -13,7 +13,7 @@ public class SpringBootController {
 
 	@RequestMapping("/")
 	public City home() {
-		// test conflict 333
+		// conflict 111
 		String name = "北京市";
 		City city = cityMapper.find(name);
 		return city;

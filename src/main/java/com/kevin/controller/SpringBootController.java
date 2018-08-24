e7f84e76d70f457d34c9c6472eb268481db5c6cd
qingwei201314 <87863823@qq.com>
2018-08-24 11:45:55 +0800

    Merge branch 'company' of https://github.com/qingwei201314/springboot-mybatis into company
    # Conflicts:
    #	src/main/java/com/kevin/controller/SpringBootController.java

--- src/main/java/com/kevin/controller/SpringBootController.java
+++ src/main/java/com/kevin/controller/SpringBootController.java
@@ -13,7 +13,7 @@ public class SpringBootController {
 
 	@RequestMapping("/")
 	public City home() {
-		// conflict 111
+		// conflict 333
 		String name = "北京市";
 		City city = cityMapper.find(name);
 		return city;
